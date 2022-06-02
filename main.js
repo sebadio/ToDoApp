191c4b5aee152dfee31d1f0a3312bac2614b7dbf
Shivu-a <sebaingame@gmail.com>
2022-06-02 12:17:17 -0300

    Agregado boton "remover todas" y prettier format

--- main.js
+++ main.js
@@ -6,39 +6,63 @@ const ul = document.querySelector("ul");
 const msg = document.querySelector(".msg");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let texto = input.value;
+  e.preventDefault();
+  let texto = input.value;
 
-    if (texto !== "") {
-        let li = document.createElement("li");
-        let p = document.createElement("p");
+  if (texto !== "") {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
 
-        p.textContent = texto;
+    p.textContent = texto;
 
-        li.appendChild(p);
-        ul.appendChild(li);
-        li.appendChild(agregarBotonBorrado());
+    li.appendChild(p);
+    ul.appendChild(li);
+    li.appendChild(agregarBotonBorrado());
 
-        input.value = "";
-        msg.style.display = "none";
+    if (ul.children.length === 1 && tareas.children.length === 1) {
+      tareas.appendChild(borrarTodos());
     }
+
+    input.value = "";
+    msg.style.display = "none";
+  }
 });
 
 function agregarBotonBorrado() {
-    let deleteButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
 
-    deleteButton.textContent = "X";
-    deleteButton.className = "deleteButton";
+  deleteButton.textContent = "X";
+  deleteButton.className = "deleteButton";
 
-    deleteButton.addEventListener("click", (e) => {
-        const item = e.target.parentElement;
-        ul.removeChild(item);
+  deleteButton.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
 
-        const items = document.querySelectorAll("li");
+    if (document.querySelectorAll("li").length === 0) {
+      msg.style.display = "block";
+      tareas.removeChild(tareas.lastElementChild);
+    }
+  });
+  return deleteButton;
+}
 
-        if (items.length === 0) {
-            msg.style.display = "block";
-        }
-    });
-    return deleteButton;
+function borrarTodos() {
+  let borrarTodos = document.createElement("button");
+
+  borrarTodos.className = "botonBorrarTodo";
+  borrarTodos.textContent = "Borrar todas las tareas";
+
+  borrarTodos.addEventListener("click", () => {
+    do {
+      ul.removeChild(ul.firstChild);
+    } while (ul.children.length !== 0);
+
+    if (document.querySelectorAll("li").length === 0) {
+      tareas.removeChild(borrarTodos);
+      msg.style.display = "block";
+    } else {
+      borrarTodos.style.display = "block";
+    }
+  });
+  return borrarTodos;
 }
